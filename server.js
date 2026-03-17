bd697be62093ff41e92ea6b000fd3f719093e606
Ghanshyam-9946 <dhoteghanshyam9@gmail.com>
2026-03-17 10:26:58 +0530

    final

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const connectToDB = require("./src/config/database")
 
 connectToDB()
 
+app.get('/get', (req,res)=>{
+    res.send("hello api")
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
