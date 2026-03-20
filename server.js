e5d34857fed65afa9351fd9a8de40de27bf65ac6
Ghanshyam-9946 <dhoteghanshyam9@gmail.com>
2026-03-20 18:03:17 +0530

    updated server

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
 connectToDB()
-
+const PORT = process.env.PORT || 3000
 app.get('/get', (req,res)=>{
     res.send("hello api")
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
