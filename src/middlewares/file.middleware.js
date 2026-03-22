32bb61e834b6c2de30f0d3ad5c161e3420137c59
Ghanshyam-9946 <dhoteghanshyam9@gmail.com>
2026-03-22 19:23:13 +0530

    middle

--- src/middlewares/file.middleware.js
+++ src/middlewares/file.middleware.js
@@ -3,13 +3,8 @@ const multer = require("multer");
 const upload = multer({
     storage: multer.memoryStorage(),
     limits: {
-        fileSize: 5 * 1024 * 1024
-    },
-    fileFilter: (req, file, cb) => {
-        if (file.mimetype === "application/pdf") {
-            cb(null, true);
-        } else {
-            cb(new Error("Only PDF allowed"), false);
-        }
+        fileSize: 10 * 1024 * 1024
     }
-})
+});
+
+module.exports = upload;
